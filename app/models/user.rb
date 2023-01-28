0872ba6c050be568c15b7142d06fcb71e9624b80
EC2 Default User <ec2-user@ip-172-31-14-145.ap-northeast-1.compute.internal>
2023-01-28 09:14:59 +0000

    [Add] 応用課題4 フォロー数表示、リンク実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,26 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
+
+
+
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
