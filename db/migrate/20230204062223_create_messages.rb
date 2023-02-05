ec3ed0b1047ef830fa48cdaf3955692bfe3c7728
EC2 Default User <ec2-user@ip-172-31-14-145.ap-northeast-1.compute.internal>
2023-02-05 01:53:16 +0000

    [Add] 応用課題8a commit-3

--- db/migrate/20230204062223_create_messages.rb
+++ db/migrate/20230204062223_create_messages.rb
@@ -3,6 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.1]
     create_table :messages do |t|
       t.integer :user_id
       t.integer :room_id
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2023_02_04_062223) do
   create_table "messages", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
