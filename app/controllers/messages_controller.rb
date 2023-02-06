92f9c2f57ed82506557e5d3ba4005562edc2ece1
EC2 Default User <ec2-user@ip-172-31-14-145.ap-northeast-1.compute.internal>
2023-02-06 11:15:24 +0000

    [Add] 応用課題8a DMチャットルーム編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,15 @@ class MessagesController < ApplicationController
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.new(message_params)
       if @message.save
-        redirect_to request.referer, notice: "You have sent message successfully."
-      else
         redirect_to request.referer
+      else
+        redirect_to request.referer, notice: "You have failed to send message."
       end
     else
       redirect_to request.referer, notice: "Error."
     end
   end
-
+  
   private
   def message_params
     params.require(:message).permit(:user_id, :room_id, :content).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
     get :follows, on: :member
     get :followers, on: :member
   end
-  
+
   # DM機能
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show]
-  
-  
+
+
   get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
