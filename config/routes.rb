7060977acac0025cedfa2925dee04b6aaba1d27b
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-02-27 11:54:49 +0000

    [Add]ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  
+  # ゲストログイン用
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
